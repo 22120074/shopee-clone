d4704913a0ba04525c741bac1b05c8781b0584c2
WhoamiSexy <22120074@student.hcmus.edu.vn>
2025-05-28 00:07:38 +0700

    Đang làm phần danh mục sản phẩm

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -43,11 +43,54 @@ function Home() {
         </div>
         {/* Phần hiển thị danh mục quảng cáo */}
         <div className='home-events-banner_list'>
-        
+          <Link to='/' className='home-events-banner_list-item'>
+            <div className='home-events-banner_list-img' 
+              style={{ backgroundImage: `url(https://cf.shopee.vn/file/vn-50009109-8a387d78a7ad954ec489d3ef9abd60b4_xhdpi)`,
+              backgroundSize: 'contain', 
+              backgroundRepeat: 'no-repeat' }}
+            />
+            <h3 className='home-events-banner_list-text'>Mã Giảm Giá</h3>
+          </Link>
+          <Link to='/' className='home-events-banner_list-item'>
+            <div className='home-events-banner_list-img' 
+              style={{ backgroundImage: `url(https://cf.shopee.vn/file/vn-50009109-f692e9b0be05d1a11cded7f9f72b5f0b_xhdpi)`,
+              backgroundSize: 'contain', 
+              backgroundRepeat: 'no-repeat' }}
+            />
+            <h3 className='home-events-banner_list-text'>Khách Hàng Thân Thiết</h3>
+          </Link>
+          <Link to='/' className='home-events-banner_list-item'>
+            <div className='home-events-banner_list-img' 
+              style={{ backgroundImage: `url(https://cf.shopee.vn/file/vn-50009109-5bf65d4dc0eb8f6b42074751e8b736a7_xhdpi)`,
+              backgroundSize: 'contain', 
+              backgroundRepeat: 'no-repeat' }}
+            />
+            <h3 className='home-events-banner_list-text'>Hàng Chọn Giá Hời</h3>
+          </Link>
+
+          <Link to='/' className='home-events-banner_list-item'>
+            <div className='home-events-banner_list-img' 
+              style={{ backgroundImage: `url(https://cf.shopee.vn/file/vn-50009109-c02353c969d19918c53deaa4ea15bdbe_xhdpi)`,
+              backgroundSize: 'contain', 
+              backgroundRepeat: 'no-repeat' }}
+            />
+            <h3 className='home-events-banner_list-text'>Shopee Style Voucher 30%</h3>
+          </Link>
+          <Link to='/' className='home-events-banner_list-item'>
+            <div className='home-events-banner_list-img' 
+              style={{ backgroundImage: `url(https://cf.shopee.vn/file/1d25d74d6900b85cfde8f967e613041d_xhdpi)`,
+              backgroundSize: 'contain', 
+              backgroundRepeat: 'no-repeat' }}
+            />
+            <h3 className='home-events-banner_list-text'>Săn Ngay 100.000 Xu</h3>
+          </Link>
         </div>
-
       </div>
-
+      {/* Danh mục sản phẩm */}
+      <div className='home-category'>
+        <h2 className='home-category-title'>Danh mục sản phẩm</h2>
+        <div></div>
+      </div>
     </div>
   );
 }
